e1f88cea828f75e83d8aba606951ffa4508b9f7a
Or Almer <almer1or@gmail.com>
2024-06-04 00:08:10 +0300

    add watermark, tomorrow will need to fix code sampling

--- PRC.py
+++ PRC.py
@@ -16,11 +16,11 @@ class PRC:
     failure_rate: float
 
     @property
-    def word_length(self):
+    def word_length(self) -> int:
         return self.G.shape[0]
 
     @property
-    def num_conditions(self):
+    def num_conditions(self) -> int:
         return self.P.shape[0]
 
     @classmethod
@@ -32,12 +32,13 @@ class PRC:
         condition_sparseness: int,
         noise_probability: float,
         failure_rate: float,
-    ) -> (np.array, np.array):
+    ) -> "PRC":
         rng = default_rng()
         P = binary_field.Zeros((num_conditions, word_length), dtype=np.int8)
         for i in range(num_conditions):
             P[i, rng.choice(word_length, size=condition_sparseness, replace=False)] = 1
         null_basis = P.null_space()
+        assert null_basis.shape[0] > random_bits / 2
         G = binary_field.Zeros((word_length, random_bits))
         for i in range(random_bits):
             rand_vec = binary_field.Random(null_basis.shape[0])
@@ -52,7 +53,7 @@ class PRC:
             failure_rate=failure_rate,
         )
 
-    def generate_codeword(self):
+    def generate_codeword(self) -> GF:
         u = binary_field.Random(self.G.shape[1])
         e = binary_field(
             np.random.choice(
@@ -63,16 +64,53 @@ class PRC:
         )
         return (self.G @ u) + self.z + e
 
-    def validate_codeword(self, x: GF):
-        return self.score_codeword(x) < self.failure_rate * self.P.shape[0]
+    def validate_codeword(self, x: GF) -> bool:
+        return self.score_codeword(x) < self.failure_rate * self.num_conditions
 
-    def score_codeword(self, x: GF):
+    def score_codeword(self, x: GF) -> float:
         return np.array((self.P @ x) + (self.P @ self.z)).sum()
 
 
-if __name__ == "__main__":
-    for i in range(20):
-        prc = PRC.generate_from_params(50, 10, 50, 5, 0, 0.1)
+def get_success_ratio(prc: PRC, num_checks=100) -> float:
+    successes = 0
+    fails = 0
+    for i in range(num_checks):
         word = prc.generate_codeword()
-        rand = binary_field.Random(prc.G.shape[0])
-        print(prc.validate_codeword(word), prc.validate_codeword(rand))
+        rand = binary_field.Random(prc.word_length)
+        successes += int(prc.validate_codeword(word))
+        fails += int(prc.validate_codeword(rand))
+    return successes / (fails + successes + 1)
+
+
+if __name__ == "__main__":
+    import nevergrad as ng
+
+    def target(
+        random_bits: int,
+        num_conditions: int,
+        condition_sparseness: int,
+    ):
+        # it's negative
+        return -get_success_ratio(
+            PRC.generate_from_params(
+                16 * 8, random_bits, num_conditions, condition_sparseness, 0.4, 0.45
+            )
+        )
+
+    # Discrete, ordered
+    random_bits = ng.p.TransitionChoice(range(5, 70))
+    num_conditions = ng.p.TransitionChoice(range(16, 70))
+    condition_sparseness = ng.p.TransitionChoice(range(3, 17, 2))
+    params = ng.p.Instrumentation(random_bits, num_conditions, condition_sparseness)
+    optimizer = ng.optimizers.DiscreteOnePlusOne(
+        parametrization=params, budget=300, num_workers=1
+    )
+
+    for _ in range(optimizer.budget):
+        x = optimizer.ask()
+        loss = target(*x.value[0])
+        print(loss, x.value[0])
+        optimizer.tell(x, loss)
+
+    recommendation = optimizer.provide_recommendation()
+    print(recommendation.value, recommendation.loss)
---
+++ PRC_watermark.py
@@ -0,0 +1,111 @@
+import random
+from math import ceil
+
+import numpy as np
+import torch
+
+from PRC import PRC, binary_field
+from utils import tokenize, binarize_setup, binarize_next, detokenize, start_model
+
+
+class PRC_watermark:
+    def __init__(self, prc: PRC, model, tokenizer):
+        self.prc = prc
+        self.model = model
+        self.tokenizer = tokenizer
+
+    def generate(self, prompt: str, length: int, encode_random=False) -> str:
+        prompt = tokenize(prompt, self.tokenizer)
+        inputs = prompt.to(self.model.device)
+        attn = torch.ones_like(inputs)
+        blen, token_to_id, id_to_token = binarize_setup(self.tokenizer)
+        past = None
+        vocab_size = len(self.tokenizer)
+        curr_word = []
+        for i in range(length):
+            if len(curr_word) == 0:
+                curr_word = np.array(self.prc.generate_codeword())
+            with torch.no_grad():
+                if past:
+                    output = self.model(
+                        inputs[:, -1:], past_key_values=past, attention_mask=attn
+                    )
+                else:
+                    output = self.model(inputs)
+
+            probs = torch.nn.functional.softmax(
+                output.logits[:, -1, :vocab_size], dim=-1
+            ).cpu()[0, :]
+            token_id = 0
+            for ind in range(blen):
+                p0, p1 = binarize_next(probs, ind, blen, token_id)
+                token_id = token_id << 1
+                # For robustness, We don't want to re-draw a
+                # codeword in the middle of a token.
+                if len(curr_word) > 0 and not encode_random:
+                    p1_hat = p1 / (p0 + p1)
+                    prob = p1_hat - (((-1) ** curr_word[0]) * min(p1_hat, 1 - p1_hat))
+                    if random.random() < prob:
+                        token_id += 1
+                    curr_word = curr_word[1:]
+                elif (random.random()) < p1 / (p0 + p1):
+                    token_id += 1
+
+            token = torch.tensor([[token_id]])
+            inputs = torch.cat([inputs, token], dim=-1)
+
+            past = output.past_key_values
+            attn = torch.cat([attn, attn.new_ones((attn.shape[0], 1))], dim=-1)
+
+        return detokenize(inputs.detach().cpu()[0], self.tokenizer)
+
+    def detect_watermark(self, text: str) -> bool:
+        blen, token_to_id, id_to_token = binarize_setup(self.tokenizer)
+        tokens = tokenize(text, self.tokenizer)[0]
+        binarized = []
+        for token in tokens:
+            # Format `tokens[i]` into a binary string with `blen` bits - 0 padded.
+            token_bits = f"{token:0{blen}b}"
+            binarized.append(
+                np.array(
+                    [1 if digit == "1" else 0 for digit in token_bits], dtype=np.int8
+                )
+            )
+        binarized = np.array(binarized)
+        for binarized_tokens_list in np.lib.stride_tricks.sliding_window_view(
+            binarized, (ceil(self.prc.word_length / blen), blen)
+        ):
+            code_word = np.concatenate(binarized_tokens_list[0])[: self.prc.word_length]
+            code_word = binary_field(code_word)
+            print(np.array(code_word).sum())
+            if self.prc.validate_codeword(code_word):
+                return True
+        return False
+
+
+if __name__ == "__main__":
+    model, tokenizer = start_model("gpt2")
+    prompts = [  # Taken from the GPT-2 official example prompts https://openai.com/research/better-language-models
+        "In a shocking finding, scientist discovered a herd of unicorns living in a remote, previously unexplored valley, in the Andes Mountains. Even more surprising to the researchers was the fact that the unicorns spoke perfect English.",
+        "A train carriage containing controlled nuclear materials was stolen in Cincinnati today. Its whereabouts are unknown.",
+        "Miley Cyrus was caught shoplifting from Abercrombie and Fitch on Hollywood Boulevard today.",
+        "Legolas and Gimli advanced on the orcs, raising their weapons with a harrowing war cry.",
+        "For today's homework assignment, please describe the reasons for the US Civil War.",
+        "John F. Kennedy was just elected President of the United States after rising from the grave decades after his assassination. Due to miraculous developments in nanotechnology, Kennedy's brain was rebuilt from his remains and installed in the control center of a state-of-the art humanoid robot. Below is a transcript of his acceptance speech.",
+    ]
+    response_sizes = [20]
+    samples_per_size = 20  # Set to 10 for a quicker run
+    watermarker = PRC_watermark(
+        PRC.generate_from_params(16 * 4, 20, 40, 3, 0, 0.4), model, tokenizer
+    )
+    for size in response_sizes:
+        total_successes = 0
+        print("Making samples of size " + str(size) + ":")
+        for i in range(samples_per_size):
+            key = random.random()
+            prompt = random.choice(prompts)
+            res = watermarker.generate(prompt=prompt, length=size, encode_random=True)
+            detected = watermarker.detect_watermark(res[len(prompt) :])
+            print(f"Run ended with {detected=}")
+            total_successes += int(detected)
+        print(f"Succeeded in {total_successes} / {samples_per_size} attempts.")
